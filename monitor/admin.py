bfca702bcaa2b239cc3b19967e276a979411a448
Ubuntu <ubuntu@ip-172-31-22-132.us-west-2.compute.internal>
2014-12-03 12:56:08 +0000

    deleted wrong files

--- monitor/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from monitor.models import Flight, FlightAdmin
-# Register your models here.
-
-admin.site.register(Flight,FlightAdmin)
--- monitor/serializers.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from rest_framework import serializers
-from monitor.models import Flight
-
-class FlightSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Flight
-        fields = ('departDate','returnDate','origin','destination', 'price', 'searchUrl','timestamp')
